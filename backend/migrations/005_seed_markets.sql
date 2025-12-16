82504b88d7cfda7cf572d913fc2ff735ff374c5d
theothegoth <huseyinguven24@hotmail.com>
2025-12-16 20:51:20 +0300

    Add SQL migration to seed markets table

---
+++ backend/migrations/005_seed_markets.sql
@@ -0,0 +1,13 @@
+-- Seed markets table with Turkish grocery markets
+INSERT INTO markets (name, base_url, delivery_regions, min_order_amount, delivery_fee, free_delivery_threshold)
+VALUES
+  ('Migros', 'https://www.migros.com.tr', NULL, 0, NULL, NULL),
+  ('A101', 'https://www.a101.com.tr', NULL, 0, NULL, NULL),
+  ('Bim', 'https://www.bim.com.tr', NULL, 0, NULL, NULL),
+  ('CarrefourSA', 'https://www.carrefoursa.com', NULL, 0, NULL, NULL),
+  ('Getir', 'https://www.getir.com', NULL, 0, NULL, NULL),
+  ('Şok', 'https://www.sokmarket.com.tr', NULL, 0, NULL, NULL),
+  ('Happy Center', 'https://www.happycenter.com.tr', NULL, 0, NULL, NULL),
+  ('Macro Center', 'https://www.macrocenter.com.tr', NULL, 0, NULL, NULL)
+ON CONFLICT (name) DO NOTHING;
+
